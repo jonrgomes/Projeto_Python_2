b456e360956eea07dd04f4eea9a4a8f177f068d1
jonrgomes <jonrgomes@gmail.com>
2026-06-24 21:46:04 -0300

    Meu primeiro commit

---
+++ desafios_MUNDO_1.py
@@ -0,0 +1,13 @@
+#DESAFIO_01
+nome = input("Qual o seu nome?")
+print('Olá,', nome,'muito prazer em conhece-lo!')
+#DESAFIO_02
+dia = input('Em que dia você nasceu?')
+Mês = input('Qual o mês?')
+Ano = input('Qual o ano?')
+print('Você nasceu em', dia, 'de', Mês, 'de', Ano,'!')
+#DESAFIO_03
+vlr1 = int(input("Digite um valor:"))
+vlr2 = int(input("Digite o segundo valor:"))
+rst = vlr1 + vlr2
+print('A soma entre', vlr1, "e", vlr2, "é", rst,"!")
