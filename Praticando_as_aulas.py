ad125da6fcfce885d3f6578131c44cf9d3f52d13
jonrgomes <jonrgomes@gmail.com>
2026-06-26 16:27:10 -0300

    Mundo_1_resoluções

--- Praticando_as_aulas.py
+++ Praticando_as_aulas.py
@@ -1,7 +1,16 @@
-#Tipos primitivos
+# %% Tópico: Primeiro comandos:
+print('Olá, Mundo!')
+
+# %% Tópico: Comprimentando
+n = input('Qual é o seu nome?')
+print(f'Prazer em te conhecer, {n}!')
+
+# %% Tópico: Tipos primitivos
 n1 = int(input('Digite um valor:'))
 n2 = int(input('Digite outro valor:'))
 s = n1 + n2
 print(f'A soma entre {n1} e {n2} vale {s}!')
 a = str(input('Digite algo:'))
 print(a.isalpha())
+
+# %% Tópico: Operações Aritimeticas
