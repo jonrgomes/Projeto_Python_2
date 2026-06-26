15e828426cbeefce6d38c7c69e54937eecd68357
jonrgomes <jonrgomes@gmail.com>
2026-06-26 16:08:11 -0300

    Mundo_1_resoluções

--- Exer_003_Mundo_01.py
+++ Exer_003_Mundo_01.py
@@ -3,3 +3,4 @@ v2 = int(input('Digite outro valor:'))
 s = v1+v2
 print(f'A soma entre {v1} e {v2} corresponde a {s}!')
 
+
--- Praticando_as_aulas.py
+++ Praticando_as_aulas.py
@@ -3,4 +3,5 @@ n1 = int(input('Digite um valor:'))
 n2 = int(input('Digite outro valor:'))
 s = n1 + n2
 print(f'A soma entre {n1} e {n2} vale {s}!')
-
+a = str(input('Digite algo:'))
+print(a.isalpha())
